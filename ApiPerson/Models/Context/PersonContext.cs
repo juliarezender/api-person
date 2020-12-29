8e67b571d010d920f916901267109b8e8b786064
juliarezender <juliarezender@gmail.com>
2020-12-29 13:57:41 -0300

    implement context

---
+++ ApiPerson/Models/Context/PersonContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiPerson.Models.Context
+{
+    public class PersonContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
--- ApiPerson/Startup.cs
+++ ApiPerson/Startup.cs
@@ -20,6 +20,8 @@ namespace ApiPerson
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PersonContext>(opt => opt.UseInMemoryDatabase("Database"));
+
             services.AddControllers();
 
             // Dependency Injection
